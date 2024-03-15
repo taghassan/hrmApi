d252997dd4c61fcee1860ecfa2511cf3be46e34e
taghassan54 <41596304+taghassan54@users.noreply.github.com>
2024-03-15 14:30:04 +0400

    add server js

--- config/server.js
+++ config/server.js
@@ -8,3 +8,16 @@ module.exports = ({ env }) => ({
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+// 	 url: 'https://strapi.syscodeia.ae',
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
