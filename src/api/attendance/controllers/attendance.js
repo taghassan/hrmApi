02958fb72df45791501d8c825e3657c1c8dd7f22
taghassan54 <41596304+taghassan54@users.noreply.github.com>
2024-04-04 17:43:59 +0400

    Update attendance.js

--- src/api/attendance/controllers/attendance.js
+++ src/api/attendance/controllers/attendance.js
@@ -254,10 +254,10 @@ module.exports = createCoreController('api::attendance.attendance', ({strapi}) =
         /**********************************************************/
         /** check is past  **/
         /**********************************************************/
-        // const todayCheckInAttendance1 = results.filter(attendance => (attendance.date && format(attendance.date, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd')) && attendance.type === `${checkIn_KEY}`)
-        // const todayCheckOutAttendance1 = results.filter(attendance => (attendance.date && format(attendance.date, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd')) && attendance.type === `${checkOut_KEY}`)
-const d=results.filter(attendance => (attendance.date && (format(attendance.date, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd')) ) && (attendance.type === `${checkIn_KEY}`))
-        return  {d,results, pagination,allDaysInMonth}
+        const todayCheckInAttendance1 = results.filter(attendance => (attendance.date && format(attendance.date, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd')) )
+        const todayCheckOutAttendance1 = results.filter(attendance => (attendance.date && format(attendance.date, 'yyyy-MM-dd') === format(day, 'yyyy-MM-dd')) )
+
+        return  {todayCheckInAttendance1,todayCheckOutAttendance1,results, pagination,allDaysInMonth}
 
         if (format(day, 'yyyy-MM-dd') <= format(now, 'yyyy-MM-dd')) {
 
