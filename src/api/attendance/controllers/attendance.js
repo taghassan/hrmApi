170152cebfaff34b61ce5409490521f86b060618
taghassan54 <41596304+taghassan54@users.noreply.github.com>
2024-04-05 06:24:02 +0400

    Update attendance.js

--- src/api/attendance/controllers/attendance.js
+++ src/api/attendance/controllers/attendance.js
@@ -254,7 +254,7 @@ module.exports = createCoreController('api::attendance.attendance', ({strapi}) =
 
       const {results, pagination} = await strapi
         .service("api::attendance.attendance").find(sanitizedQueryParams)
-
+return {results, pagination}
       const outputArr = [];
 
 
